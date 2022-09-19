58dcf1426a4fb5e6c4a8dab784d2780113cc65e7
Anush79 <66175237+Anush79@users.noreply.github.com>
2022-09-19 20:19:00 +0530

    everything checked

---
+++ app.js
@@ -0,0 +1,37 @@
+var serverurl = "https://api.funtranslations.com/translate/minion.json";
+
+var textInput = document.querySelector("#input_text");
+var btntranslate = document.querySelector("#btn_translate");
+var textOutput = document.querySelector("#translated");
+var pickaboo = document.querySelector("#pickaboo");
+const song = new Audio("sound/banana_song.mp3");
+const hello = new Audio("sound/hello.mp3");
+
+function errorhandle(error) {
+  console.log("Some Error Occured ", error);
+  alert("Something went wrong, Please try again later");
+}
+
+function translatedURL(text) {
+  return serverurl + "?" + "text=" + text;
+}
+
+function onclick() {
+  var input = textInput.value;
+  pickaboo.style.visibility = "visible";
+  pickaboo.style.animationName = "pickaboo";
+  hello.play();
+
+  fetch(translatedURL(input))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedtext = json.contents.translated;
+      textOutput.innerText = translatedtext;
+    })
+    .catch(errorhandle);
+}
+
+function banana_song() {
+  song.play();
+}
+btntranslate.addEventListener("click", onclick);
